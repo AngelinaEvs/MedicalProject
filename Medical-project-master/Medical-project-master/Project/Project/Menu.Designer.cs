92bc7bbf41f94278adb7672fcbd10c20809e965e
AngelinaEvs <57727402+AngelinaEvs@users.noreply.github.com>
2020-05-14 22:13:52 +0300

    Project

---
+++ Medical-project-master/Medical-project-master/Project/Project/Menu.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Project
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PatientListButton = new System.Windows.Forms.Button();
+            this.addAnalysButton = new System.Windows.Forms.Button();
+            this.addPatientButton = new System.Windows.Forms.Button();
+            this.PatienName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Patronymic = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sitizenShip = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.passportData = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastSurvey = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeight = 50;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PatienName,
+            this.fName,
+            this.Patronymic,
+            this.sitizenShip,
+            this.passportData,
+            this.lastSurvey});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Aquamarine;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.WindowText;
+            this.dataGridView1.Location = new System.Drawing.Point(200, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.RowTemplate.Height = 40;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(949, 549);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1161, 83);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SimSun", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.label1.Location = new System.Drawing.Point(21, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project ";
+            // 
+            // PatientListButton
+            // 
+            this.PatientListButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.PatientListButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PatientListButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PatientListButton.ForeColor = System.Drawing.Color.FloralWhite;
+            this.PatientListButton.Location = new System.Drawing.Point(23, 133);
+            this.PatientListButton.Name = "PatientListButton";
+            this.PatientListButton.Size = new System.Drawing.Size(138, 56);
+            this.PatientListButton.TabIndex = 6;
+            this.PatientListButton.Text = "Обновить список";
+            this.PatientListButton.UseVisualStyleBackColor = false;
+            this.PatientListButton.Click += new System.EventHandler(this.PatientListButton_Click_1);
+            // 
+            // addAnalysButton
+            // 
+            this.addAnalysButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.addAnalysButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.addAnalysButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addAnalysButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addAnalysButton.ForeColor = System.Drawing.Color.FloralWhite;
+            this.addAnalysButton.Location = new System.Drawing.Point(23, 336);
+            this.addAnalysButton.Name = "addAnalysButton";
+            this.addAnalysButton.Size = new System.Drawing.Size(138, 56);
+            this.addAnalysButton.TabIndex = 7;
+            this.addAnalysButton.Text = "Добавить анализ";
+            this.addAnalysButton.UseVisualStyleBackColor = false;
+            this.addAnalysButton.Click += new System.EventHandler(this.addAnalysButton_Click);
+            // 
+            // addPatientButton
+            // 
+            this.addPatientButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.addPatientButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addPatientButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addPatientButton.ForeColor = System.Drawing.Color.FloralWhite;
+            this.addPatientButton.Location = new System.Drawing.Point(23, 235);
+            this.addPatientButton.Name = "addPatientButton";
+            this.addPatientButton.Size = new System.Drawing.Size(138, 56);
+            this.addPatientButton.TabIndex = 8;
+            this.addPatientButton.Text = "Добавить пациента";
+            this.addPatientButton.UseVisualStyleBackColor = false;
+            this.addPatientButton.Click += new System.EventHandler(this.addPatientButton_Click);
+            // 
+            // PatienName
+            // 
+            this.PatienName.HeaderText = "Имя";
+            this.PatienName.MinimumWidth = 40;
+            this.PatienName.Name = "PatienName";
+            this.PatienName.ReadOnly = true;
+            this.PatienName.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.PatienName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.PatienName.Width = 150;
+            // 
+            // fName
+            // 
+            this.fName.HeaderText = "Фамилия";
+            this.fName.Name = "fName";
+            this.fName.ReadOnly = true;
+            this.fName.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.fName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.fName.Width = 150;
+            // 
+            // Patronymic
+            // 
+            this.Patronymic.HeaderText = "Отчество";
+            this.Patronymic.Name = "Patronymic";
+            this.Patronymic.ReadOnly = true;
+            this.Patronymic.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Patronymic.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Patronymic.Width = 150;
+            // 
+            // sitizenShip
+            // 
+            this.sitizenShip.HeaderText = "Гражданство";
+            this.sitizenShip.Name = "sitizenShip";
+            this.sitizenShip.ReadOnly = true;
+            this.sitizenShip.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.sitizenShip.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.sitizenShip.Width = 130;
+            // 
+            // passportData
+            // 
+            this.passportData.HeaderText = "Прикрепление";
+            this.passportData.Name = "passportData";
+            this.passportData.ReadOnly = true;
+            this.passportData.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.passportData.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.passportData.Width = 230;
+            // 
+            // lastSurvey
+            // 
+            this.lastSurvey.HeaderText = "Дата обследования";
+            this.lastSurvey.Name = "lastSurvey";
+            this.lastSurvey.ReadOnly = true;
+            this.lastSurvey.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.lastSurvey.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.lastSurvey.Width = 135;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1161, 648);
+            this.Controls.Add(this.addPatientButton);
+            this.Controls.Add(this.addAnalysButton);
+            this.Controls.Add(this.PatientListButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximizeBox = false;
+            this.Name = "Menu";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Button PatientListButton;
+        private System.Windows.Forms.Button addAnalysButton;
+        private System.Windows.Forms.Button addPatientButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PatienName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Patronymic;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sitizenShip;
+        private System.Windows.Forms.DataGridViewTextBoxColumn passportData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastSurvey;
+    }
+}
+
---
+++ Medical-project-master/Medical-project-master/Project/Project/Menu.cs
@@ -0,0 +1,94 @@
+﻿using Project.Classes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Menu : Form
+    {
+        private List<Patient> list;
+
+        public Menu()
+        {
+            InitializeComponent();
+            list = new List<Patient>();
+        }
+
+        public void addPatient(Patient p)
+        {
+            list.Add(p);
+            Information inf = (Information)p.getInformation();
+            String lastS = inf.lastSurvey.ToShortDateString().Equals("01.01.0001") ? "-" : inf.lastSurvey.ToShortDateString();
+            dataGridView1.Rows.Add(inf.fName,inf.lName,inf.patronymic,inf.sitizenShip,inf.attachment,lastS);
+            if(dataGridView1.Rows.Count==1)dataGridView1.ClearSelection();
+            /*
+            var l= from inf in list select inf.getInformation();
+            dataGridView1.DataSource = from inf in list select (Information)inf.getInformation();
+            */
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addPatientButton_Click(object sender, EventArgs e)
+        {
+            Profile form1 = new Profile(this);
+            form1.Show();
+        }
+        /* Profile form1 = new Profile(this);
+            form1.Show();
+            */
+
+        private void PatientListButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {   if (e.RowIndex<0) return;
+            ProfileInfo form = new ProfileInfo(list[dataGridView1.CurrentRow.Index]);
+            form.Show();
+        }
+
+        private void addAnalysButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Выберите пациента");
+                return;
+            }
+            Analysis form = new Analysis(list[dataGridView1.CurrentRow.Index]);
+            form.Show();
+        }
+
+        public void reload()
+        {
+            dataGridView1.Rows.Clear();
+            foreach(Patient p in list){
+                Information inf = (Information)p.getInformation();
+                String lastS = inf.lastSurvey.ToShortDateString().Equals("01.01.0001") ? "-" : inf.lastSurvey.ToShortDateString();
+                dataGridView1.Rows.Add(inf.fName, inf.lName, inf.patronymic, inf.sitizenShip, inf.attachment, lastS);
+                if (dataGridView1.Rows.Count == 1) dataGridView1.ClearSelection();
+            }
+
+        }
+
+        private void PatientListButton_Click_1(object sender, EventArgs e)
+        {
+            reload();
+        }
+    }
+    
+}
